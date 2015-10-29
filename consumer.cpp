e41dd22cc9eaa6e17d11b446beab0694f5351d09
Igor Alimov <alimov@corp.sputnik.ru>
2015-10-29 15:07:35 +0300

    First commit

---
+++ consumer.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file consumer.cpp
+ * @author igor
+ * @date 26 окт. 2015 г.
+ */
+
+#include <iostream>
+#include <exception>
+#include <cstdlib>
+
+
+int
+main(int argc, char *argv[])
+try
+{
+	std::cout << "Consumer" << std::endl;
+	return EXIT_SUCCESS;
+}
+catch (const std::exception& e)
+{
+	std::cerr << "Exception: " << e.what() << std::endl;
+    return EXIT_FAILURE;
+}
+catch(...)
+{
+	std::cerr << "Unknown exceprion" << std::endl;
+    return EXIT_FAILURE;
+}
